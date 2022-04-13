0e89e383888d085feb37fd9e965e7772fbd45def
KlausDk <klausdk1999@gmail.com>
2022-04-13 02:59:44 -0300

    Login de usuario, implementação do loop com falhas

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
 //elementoQueQueroQueApareca.scrollIntoView(); query selector innerhtml= entao scroll
 let user;
-
-function login(){
-    document.querySelector(".feed").innerHTML=`
-    <div class="loginScreen">
-        <img src="media/logo 1.png" alt="">
-        <input placeholder="Digite seu nome" type="text">
-        <button type="button">Entrar</button>
-    </div>`
-
+let username;
+let array=[];
+function joinChat(){
+    username=document.getElementById("usernameInput").value;
+    let data={
+        name: username
+    };
+    let promise=axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',data);
+    promise.then(success);
+    promise.catch(fail);
 }
-function join(){
+function fail(error){
+    console.log(error.response.status);
+    if(error.response.status==400){
+        alert(`Erro no login, usuário ja está em uso. Por favor escolha outro nome de usuário`);
+    }
+}
+function success(){
+    document.getElementById("loginScreen").style.display="none";
+    console.log("success");
+    joinHTML();
+    privateHTML();
+    joinHTML();
+    privateHTML();
+    messageAllHTML();
+    messageAllHTML();
+    loop();
+}
+function loop(){
+    setTimeout(function(){
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',data);
+        array = axios.get('http://mock-api.driven.com.br/api/v6/uol/messages');
+        loop();
+        printMessages();
+    }, 5000); 
+}
+function printMessages(){
+    for(let i=0;i<array.length;i++){
+        console.log(array[0].type);
+    }
+}
+function joinHTML(){
     document.querySelector(".feed").innerHTML+=`
     <div class="messageBox grey">
         <h1><strong>fulano</strong> entrou no chat</h1>
     </div>`
 }
-function leave(){
+function leaveHTML(){
     document.querySelector(".feed").innerHTML+=`
     <div class="messageBox grey">
         <h1><strong>fulano</strong> saiu do chat</h1>
     </div>`
 }
-function private(){
+function privateHTML(){
     document.querySelector(".feed").innerHTML+=`
     <div class="messageBox pink">
-        <h1><strong>klaus</strong> reservadamente para <strong>ciclano</strong>: mensagem </h1>
+        <h1><strong>klaus</strong> reservadamente para <strong>Carol</strong>: Ola gata, vamos tc?</h1>
     </div>`
 }
-function messageAll(){
+function messageAllHTML(){
     document.querySelector(".feed").innerHTML+=`
     <div class="messageBox white">
         <h1><strong>fulano</strong> para <strong>todos</strong>: mensagem </h1>
     </div>`
 }
 
-login();
-// private();
-// messageAll();
-// leave();
-// join();
-// join();
-// private();
-// messageAll();
+

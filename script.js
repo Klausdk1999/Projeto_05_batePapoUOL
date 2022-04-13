1a5ad54d6751e8dd8e8d87242d44d534d0aa75e1
KlausDk <klausdk1999@gmail.com>
2022-04-13 14:46:57 -0300

    Seleção de mensagem privada, erro no checkmark

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//elementoQueQueroQueApareca.scrollIntoView(); query selector innerhtml= entao scroll
 let user;
 let username;
+let messageType="message";
+let messageTo="Todos";
 let data={
     name: ""
 };
@@ -15,20 +16,12 @@ function joinChat(){
     promise.catch(fail);
 }
 function fail(error){
-    console.log(error.response.status);
     if(error.response.status==400){
         alert(`Erro no login, usuário ja está em uso. Por favor escolha outro nome de usuário`);
     }
 }
 function success(){
     document.getElementById("loginScreen").style.display="none";
-    console.log("success");
-    // joinHTML();
-    // privateHTML();
-    // joinHTML();
-    // privateHTML();
-    // messageAllHTML();
-    // messageAllHTML();
     loop();
 }
 function loop(){
@@ -80,9 +73,40 @@ function sendMessage(){
     let messageToSend=document.querySelector(".message").value;
     let dataSendMessage={
         from: username,
-        to: "Todos",
+        to: messageTo,
         text: messageToSend,
-        type: "message" // ou "private_message" para o bônus
+        type: messageType // ou "private_message" para o bônus
     };
     axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',dataSendMessage);
 }
+function sendScreen(){
+    let promise=axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    document.querySelector(".sendScreen").classList.toggle("displayNone");
+    promise.then(listHTML);
+}
+function listHTML(user){
+    console.log(user.data[0]);
+    for(let i=0;i<user.data.length;i++){
+        document.querySelector(".userList").innerHTML+=`
+        <div class="spaceBetween" onclick="selectUserToSend(this)">
+            <div class="user"> 
+            <ion-icon name="person-circle"></ion-icon>
+            <h1> ${user.data[i].name}</h1>
+            </div>
+            <ion-icon class="displayNone"id="checkmark" name="checkmark"></ion-icon>
+        </div>`;
+    } 
+}
+function selectUserToSend(element){
+    element.querySelector(".checkmark").classList.toggle("checkmark");
+}
+function private(element){
+    selectUserToSend(element);
+    messageType="private_message";
+}
+function public(element){
+    selectUserToSend(element);
+    messageType="message";
+}
+
+
